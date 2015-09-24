edb13840dfb5dab096af502312b7cd4191a89d29
JonnyDaenen <jonnydaenen@gmail.com>
2015-09-24 09:17:02 +0200

    added postgres connection

--- exptester.py
+++ exptester.py
@@ -1,6 +1,9 @@
 from mrsettings import MR_settings
 from simulator import MR_cost_model
 
+import psycopg2
+import psycopg2.extras
+
 __author__ = 'Jonny Daenen'
 
 
@@ -8,7 +11,22 @@ if __name__ == '__main__':
 
     cost_model = MR_cost_model(MR_settings())
 
+    conn = psycopg2.connect("dbname=jonny user=jonny")
+
+    cur = conn.cursor(cursor_factory = psycopg2.extras.NamedTupleCursor)
+    cur.execute("SELECT job, cpu_millis, map_millis, red_millis, hdfs_bytes_read, map_output_bytes FROM jobs;")
+
     # for each job
+    for record in cur:
+        cost = cost_model.get_mr_cost(record.hdfs_bytes_read,record.map_output_bytes,False,False)
+        print record.job, record.cpu_millis, record.map_millis, record.red_millis, cost[0], cost[1], sum(cost)
+
+
+    cur.close()
+
+    conn.close()
+
+
 
         # get input data (hdfs reads)
         # get intermediate data (map output bytes)
---
+++ readme.md
@@ -0,0 +1,19 @@
+
+# MR grouping simulation
+
+Python script to test the correctness of a grouping policy.
+
+## Dependencies
+
+- Psycopg
+
+## version history
+
+### v0.2
+
+- added settings class
+- added experiment tester (connection with Postgres DB)
+
+### v0.1
+
+- basic cost calculation script
